e24d176122faca03c09b9bc41e14d3f59935d0b0
Lelita <lelitavikri33@gmail.com>
2022-04-09 09:54:24 +0800

    Commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.scss";
+
+
+
+export default function Footer() {
+
+    return(
+        <div className="footer">
+                @ 2022, Final Project by Bhakti Buana and Lelita Vikri
+        </div>
+    )}
